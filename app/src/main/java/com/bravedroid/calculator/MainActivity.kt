67570a88b08adc00c6ff61347089823b51ffbb22
houssemzaier <houssem.zaier@gmail.com>
2022-12-31 18:12:02 +0100

    trigger Helloooz

--- app/src/main/java/com/bravedroid/calculator/MainActivity.kt
+++ app/src/main/java/com/bravedroid/calculator/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hellooo $name!")
+    Text(text = "Helloooz $name!")
 }
 
 @Preview(showBackground = true)
